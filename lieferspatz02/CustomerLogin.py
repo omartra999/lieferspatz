68454a862e1b47c358f6dc3ef3048177ce89fdd5
omartra999 <151784092+omartra999@users.noreply.github.com>
2023-11-24 14:58:02 +0300

    Add files via upload

---
+++ lieferspatz02/CustomerLogin.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+class customerLogin:
+    def __init__(self,connection):
+        self.connection =sqlite3.connect(connection)
+        self.cursor = self.connection.cursor()
+
+    def login(self, username, password):
+        query = "SELECT COUNT(*) FROM customer WHERE username = ? AND password = ?"
+        with self.connection:
+            result = self.cursor.execute(query,(username, password,)).fetchone()[0]
+            if result > 0:
+                return True
