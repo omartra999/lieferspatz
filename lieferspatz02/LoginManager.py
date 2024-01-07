2d64e9f1185d7ad04a32a333e4a54102ab76f486
hahaout <121407462+hahaout@users.noreply.github.com>
2024-01-07 14:17:10 +0100

    Update LoginManager.py
    return array

--- lieferspatz02/LoginManager.py
+++ lieferspatz02/LoginManager.py
@@ -6,25 +6,16 @@ class loginManager:
         self.cursor = self.connection.cursor()
 
     def loginCustomer(self, username, password):
-        customer_query = "SELECT COUNT(*) FROM customer WHERE username = ? AND password = ?"
+        customer_query = "SELECT * FROM customer WHERE username = ? AND password = ?"
         with self.connection:
             result = self.cursor.execute(customer_query, (username, password,)).fetchone()[0]
             return result 
 
     def loginRestaurant(self, username, password):
-        restarant_query = "SELECT COUNT(*), restaurantname, id, username, address, plz, email, description FROM restaurant WHERE username = ? AND password = ?"
+        restarant_query = "SELECT * FROM restaurant WHERE username = ? AND password = ?"
         with self.connection:
             result = self.cursor.execute(restarant_query, (username, password,)).fetchone()
-            auth = result[0]
-            restaurant_name = result[1]
-            restaurant_id = result[2]
-            restaurant_username = result[3]
-            restaurant_address = result[4]
-            restaurant_plz = result[5]
-            restaurant_email = result[6]
-            restaurant_description = result[7]
-
-            if auth > 0: 
-                return auth, username, restaurant_name,restaurant_id,restaurant_address, restaurant_plz, restaurant_email, restaurant_description
+            if result[0] > 0: 
+                return result
             else: 
                 return False
