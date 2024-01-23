8f04b2e314afaf916f97d308c2c2b629b33c1c49
omartra999 <151784092+omartra999@users.noreply.github.com>
2024-01-23 19:37:14 +0100

    app.py sorted menu + fixed add items bug

--- lieferspatz02/app.py
+++ lieferspatz02/app.py
@@ -535,27 +535,7 @@ def edit_range():
 @app.route("/add_items", methods = ["POST", "GET"])
 @login_required_restaurant
 def add_items():
-    restaurant_id = session['restaurant_id']
     time_manager = timeManager(connection)
-    restaurant = _restaurant(restaurant_id, connection)
-    if ('restaurant_name') in session and ('restaurant_id') in session:
-
-        menu = restaurant.getMenu()
-        delivery_range = restaurant.get_delivery_raduis()
-        opening_times = time_manager.get_openning_times(restaurant_id)
-        logo_data = restaurant.getLogo()
-        # Combine variables into a single dictionary
-        template_data = {
-            "restaurantName": session['restaurant_name'],
-            "userName": session['username'],
-            "restaurantAddress": session['address'],
-            "Postal": session['plz'],
-            "mail": session['email'],
-            "des": session['description'],
-            "items": menu,
-            "openTimes": opening_times,
-            "logo_data": logo_data
-        }
     restaurant_id = session.get('restaurant_id')
     restaurant = _restaurant(restaurant_id, connection)
     if request.method == "POST":
@@ -563,20 +543,38 @@ def add_items():
         item_name = request.form.get('item_name')
         detail = request.form.get('detail')
         price = request.form.get('price')
-        types = request.form.get('food_type')
+        types = request.form.get('types')
         logo = request.files.get('food_logo')
         print("logo:",logo)
+        
         
         
         if restaurant.add_item(item_name, detail, price, types, logo):
             items = restaurant.getMenu()
             flash("item added successfuly")
-            return render_template("restaurant_home.html", show_menu_button=False, show_menu_form=True, addedItems=items, range = restaurant.get_delivery_raduis(), **template_data)
+            template_data = {
+            "restaurantName": session['restaurant_name'],
+            "userName": session['username'],
+            "restaurantAddress": session['address'],
+            "Postal": session['plz'],
+            "mail": session['email'],
+            "des": session['description'],
+            "should_show_edit_button": False,
+            "should_show_edit_form" : False,
+            "show_menu_button": False,
+            "show_menu_form":True,
+            "addedItems": restaurant.getMenu(),
+            "range":  restaurant.get_delivery_raduis(),
+            "openTimes": time_manager.get_openning_times(restaurant_id),
+            "logo_data": restaurant.getLogo()
+        }
+            return render_template("restaurant_home.html", **template_data)
         else:
             flash("Items are not added some Error occured")
-            return render_template("restaurant_home.html", show_menu_button=False, show_menu_form=True, **template_data)
+            return render_template("restaurant_home.html", show_menu_button=False, show_menu_form=True)
     else:
-        return render_template("restaurant_home.html", show_menu_button = False, show_menu_form = True, **template_data)
+        return render_template("restaurant_home.html", show_menu_button = False, show_menu_form = True)
+
     
 @app.route("/home", methods=["GET", "POST"])
 @login_required_customer
@@ -609,24 +607,42 @@ def restaurant_menu():
     if request.method == "POST":
         restaurant_id = request.form.get("restaurant_id")
         customer_id = session.get("user_id")
-        username = session.get("username")
         session["customer_id"] = customer_id
         #restaurant instance to retrieve the menu
         restaurant = _restaurant(restaurant_id, connection)
-        menu = restaurant.getMenu()
-        selected_restaurant = f.get_information(restaurant_id,'restaurant')
-        all_menu = [] 
-        for m in menu:
-            current_menu = {
-                "menu":m,
-                "logo": restaurant.getfoodLogo(m[0])
-            }
-            all_menu.append(current_menu)
-        return render_template("restaurant_menu.html", restaurants=selected_restaurant, menus=all_menu, customer_id=customer_id, username = username)
+        _customer = customer(customer_id, connection)
+
+        types_result = restaurant.get_item_types()
+        types = [item_type[0] for item_type in types_result]
+        menu_by_type = {}
+        print("types: ", types)
+        #sorting items with thier types in a dictionary
+        for type in types:
+            items = restaurant.get_items_of_type(type)
+            menu_by_type[type] = []
+            for item in items:
+                menu_by_type[type].append({ "id": item[0], "restaurant_id":item[1] , "name":item[2], "description":item[3], "price": item[4], "logo": restaurant.getfoodLogo(item[0]) })
+        menu = menu_by_type.items()
+        
+        print("menu:", menu)
+
+        template_data = {
+            "restaurant_name": restaurant.get_restaurant_name(),
+            "description": restaurant.get_description(),
+            "restaurant_adresse": restaurant.get_address(),
+            "restaurant_plz": restaurant.get_plz(),
+            "customer_id": customer_id,
+            "customer_username": _customer.get_username(),
+            "menu": menu_by_type
+
+            
+        }
+        
+
+        return render_template("restaurant_menu.html", **template_data)
         # Fetch restaurant information based on the provided restaurant_id
     else:
         return redirect(url_for('home'))
- 
 @app.route("/delivery_timer")
 def delivery_timer():
         return render_template("delivery_timer.html")
